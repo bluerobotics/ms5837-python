c10795e0d0e81d8261d3cc1c41f353e6afa85cae
Lucien Gaitskell <lucieng@gaitskell.com>
2019-05-19 13:48:40 -0400

    Fix indentation

--- example.py
+++ example.py
@@ -45,9 +45,9 @@ while True:
         if sensor.read():
                 print("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
                     sensor.pressure(),  # Default is mbar (no arguments)
-                sensor.pressure(ms5837.UNITS_psi),  # Request psi
-                sensor.temperature(),  # Default is degrees C (no arguments)
-                sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
+                    sensor.pressure(ms5837.UNITS_psi),  # Request psi
+                    sensor.temperature(),  # Default is degrees C (no arguments)
+                    sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
         else:
                 print "Sensor read failed!"
                 exit(1)

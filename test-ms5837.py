bf0d5bc2b068314a64d0f91fcba17ef9efaf77ea
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-07 17:58:12 +1000

    test-ms5837: update to use `LLogWriter`

--- test-ms5837.py
+++ test-ms5837.py
@@ -2,48 +2,37 @@
 
 import argparse
 from ms5837 import MS5837_02BA, MS5837_30BA
-import signal
-import subprocess
+from pathlib import Path
+import llog
 import time
 
-parser = argparse.ArgumentParser()
+device = 'ms5837'
+defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
+
+parser = argparse.ArgumentParser(description=f'{device} test')
 parser.add_argument('--bar02', action='store_true',
                     help='run test for Bar02 02BA model (default is Bar30 30BA model)')
 parser.add_argument('--bus', action='store', type=int, required=True)
 parser.add_argument('--frequency', action='store', type=int,
                     default=1, help="set the measurement frequency")
+parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
 parser.add_argument('--output', action='store', type=str, default=None)
 args = parser.parse_args()
 
-if args.bar02:
-    ms = MS5837_02BA(args.bus)
-else:
-    ms = MS5837_30BA(args.bus)
-ms.init()
 
-outfile = None
-
-if args.output:
-    outfile = open(args.output, "w")
-
-
-def cleanup(_signo, _stack):
-    if outfile:
-        outfile.close()
-    exit(0)
-
-
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-while True:
-    try:
-        ms.read()
-        output = f'{time.time()} 1 {ms.pressure()} {ms.temperature()}'
-    except Exception as e:
-        output = f"{time.time()} 0 {e}"
-    print(output)
-    if outfile:
-        outfile.write(output + '\n')
-    if args.frequency:
-        time.sleep(1.0/args.frequency)
+with llog.LLogWriter(args.meta, args.output) as log:
+    if args.bar02:
+        ms = MS5837_02BA(args.bus)
+    else:
+        ms = MS5837_30BA(args.bus)
+    ms.init()
+  
+    while True:
+        try:
+            ms.read()
+            log.log(llog.LLOG_DATA, f"{ms.pressure()} {ms.temperature()}")
+        except Exception as e:
+            log.log(llog.LLOG_ERROR, e)
+      
+        if args.frequency:
+            time.sleep(1.0/args.frequency)

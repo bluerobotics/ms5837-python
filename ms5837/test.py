3227802b249d3d483027ddf59054acee7dc46e90
Jacob Walser <jwalser90@gmail.com>
2021-10-05 01:51:53 -0400

    test: update for llog

--- ms5837/test.py
+++ ms5837/test.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
 def main():
-    import argparse
     from ms5837 import MS5837_02BA, MS5837_30BA
-    import signal
-    import subprocess
-    import time
+    from llog import LLogWriter
 
-    parser = argparse.ArgumentParser()
+    device = "ms5837"
+    parser = LLogWriter.create_default_parser(__file__, device)
+    parser.add_argument("--bus", default=6, help="i2c bus")
     parser.add_argument('--bar02', action='store_true',
                         help='run test for Bar02 02BA model (default is Bar30 30BA model)')
-    parser.add_argument('--bus', action='store', type=int, required=True)
-    parser.add_argument('--frequency', action='store', type=int,
-                        default=1, help="set the measurement frequency")
-    parser.add_argument('--output', action='store', type=str, default=None)
     args = parser.parse_args()
 
     if args.bar02:
@@ -22,32 +17,12 @@ def main():
         ms = MS5837_30BA(args.bus)
     ms.init()
 
-    outfile = None
-
-    if args.output:
-        outfile = open(args.output, "w")
-
-
-    def cleanup(_signo, _stack):
-        if outfile:
-            outfile.close()
-        exit(0)
-
-
-    signal.signal(signal.SIGTERM, cleanup)
-    signal.signal(signal.SIGINT, cleanup)
-
-    while True:
-        try:
+    with LLogWriter(args.meta, args.output, console=args.console) as log:
+        def data_getter():
             ms.read()
-            output = f'{time.time()} 1 {ms.pressure()} {ms.temperature()}'
-        except Exception as e:
-            output = f"{time.time()} 0 {e}"
-        print(output)
-        if outfile:
-            outfile.write(output + '\n')
-        if args.frequency:
-            time.sleep(1.0/args.frequency)
+            return f'{ms.pressure():.6f} {ms.temperature():.6f}'
+
+        log.log_data_loop(data_getter, parser_args=args)
 
 if __name__ == '__main__':
     main()
